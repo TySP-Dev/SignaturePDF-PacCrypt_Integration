62f1b039f4fc37ca774024d724f663e7f4d5c27b
Tyler <68524461+TySP-Dev@users.noreply.github.com>
2025-05-17 23:51:57 -1000

    Update signature.html.php

--- templates/signature.html.php
+++ templates/signature.html.php
@@ -322,10 +322,7 @@
     url_font = <?php echo json_encode('/vendor/fonts/Caveat-Regular.ttf') ?>
     <?php endif; ?>
     </script>
-    <script type="module">
-        import * as PDFLib from "https://unpkg.com/pdf-lib/dist/pdf-lib.min.js";
-        window.PDFLib = PDFLib;
-    </script>
+    <script src="https://unpkg.com/pdf-lib/dist/pdf-lib.min.js"></script>
     <script src="<?php echo $REVERSE_PROXY_URL; ?>/js/common.js?<?php echo ($COMMIT) ? $COMMIT : filemtime($ROOT."/public/js/common.js") ?>"></script>
     <script src="<?php echo $REVERSE_PROXY_URL; ?>/js/signature.js?<?php echo ($COMMIT) ? $COMMIT : filemtime($ROOT."/public/js/signature.js") ?>"></script>
   </body>
